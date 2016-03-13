8c714251e83753a41980ee85b054ad7ea99c95f3
Samuele Resca <samuele.resca@gmail.com>
2016-03-13 19:48:53 +0100

    first commit

---
+++ async_chapter/async_series_base.js
@@ -0,0 +1,33 @@
+/**
+ * Created by rescas on 3/13/2016.
+ */
+
+var async = require("async");
+var results = [];
+
+async.series([
+    function (callback) {
+        setTimeout(function () {
+            console.log("Task 1");
+            callback(null, 1);
+        }, 300);
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log("Task 2");
+            callback(null, 2);
+        }, 200);
+    },
+    function (callback) {
+        setTimeout(function () {
+            console.log("Task 3");
+            callback(null, 3);
+        }, 100);
+    }
+], function (error, results) {
+    console.log(results);
+});
+
+
+
+
---
+++ async_chapter/without_async.js
@@ -0,0 +1,23 @@
+/**
+ * Created by rescas on 3/13/2016.
+ */
+
+var results = [];
+
+setTimeout(function () {
+    console.log("Task 1");
+    results[0] = 1;
+}, 300);
+
+setTimeout(function () {
+    console.log("Task 2");
+    results[1] = 2;
+}, 200);
+
+
+setTimeout(function () {
+    console.log("Task 3");
+    results[3] = 3;
+}, 100);
+
+
